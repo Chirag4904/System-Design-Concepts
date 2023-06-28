6a4aaea4a0cabc626636e0dac762faa4f0e9aa35
chiggy69 <chiragaggarwal69@gmail.com>
2023-06-28 16:39:32 +0530

    add new data storage node support added

--- Consistent Hashing/routes/nodeRoutes.js
+++ Consistent Hashing/routes/nodeRoutes.js
@@ -1,6 +1,11 @@
 import express from "express";
 import { lookUpArray, storageNodeMap } from "../utils/util.js";
-import { hash } from "../helpers/helper.js";
+import {
+	hash,
+	findNextNode,
+	findNextNodeForNewNode,
+} from "../helpers/helper.js";
+import { StorageNode } from "../utils/StorageNode.js";
 const nodeRouter = express.Router();
 
 nodeRouter.get("/:id", (req, res) => {
@@ -13,11 +18,39 @@ nodeRouter.get("/:id", (req, res) => {
 				id: value.id,
 				ip: value.ip,
 				data: plainObject,
+				hash: hash(value.ip),
 			});
 			return;
 		}
 	}
 	res.status(404).send("Node not found");
+});
+
+nodeRouter.post("/", (req, res) => {
+	const { ip } = req.body;
+	const id = storageNodeMap.size + 1;
+	const newNode = new StorageNode(id, ip);
+	const hashValue = hash(ip);
+	const isHashAlreadyPresent = lookUpArray.includes(hashValue);
+	if (isHashAlreadyPresent) {
+		res.status(409).send("Hash already present");
+		return;
+	}
+
+	const nextNode = findNextNodeForNewNode(hashValue, lookUpArray);
+	console.log(nextNode, "next node line 41");
+	//add new node to the ring
+	storageNodeMap.set(hashValue, newNode);
+	lookUpArray.push(hashValue);
+
+	//transfer data from next node to new node (only keys which new node will take)
+	for (const [key, value] of nextNode.map.entries()) {
+		if (hash(key) <= hashValue) {
+			newNode.add(key, value);
+			nextNode.remove(key);
+		}
+	}
+	res.status(200).send("Successfully added");
 });
 
 nodeRouter.delete("/", (req, res) => {
@@ -27,11 +60,12 @@ nodeRouter.delete("/", (req, res) => {
 		if (node.id === +nodeId) {
 			try {
 				const tempIndex = lookUpArray.indexOf(hash(node.ip));
-				// transfer data to the next node
-				const nextNode = storageNodeMap.get(lookUpArray[tempIndex + 1]);
+				const nextNode = findNextNode(node, lookUpArray);
+
 				for (const [key, value] of node.map.entries()) {
 					nextNode.add(key, value);
 				}
+
 				storageNodeMap.delete(lookUpArray[tempIndex]);
 				lookUpArray.splice(tempIndex, 1);
 				res.status(200).send("Successfully removed");
