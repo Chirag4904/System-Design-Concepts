309fe7b6ca0cb3f6718899f09299df40302c604b
chiggy69 <chiragaggarwal69@gmail.com>
2023-06-28 15:19:20 +0530

    removing node logic added

--- Consistent Hashing/routes/nodeRoutes.js
+++ Consistent Hashing/routes/nodeRoutes.js
@@ -1,12 +1,19 @@
 import express from "express";
 import { lookUpArray, storageNodeMap } from "../utils/util.js";
+import { hash } from "../helpers/helper.js";
 const nodeRouter = express.Router();
 
 nodeRouter.get("/:id", (req, res) => {
 	const { id } = req.params;
-	for (const values of storageNodeMap.values()) {
-		if (values.getId() === +id) {
-			res.status(200).send(values);
+	for (const value of storageNodeMap.values()) {
+		if (value.id === +id) {
+			const plainObject = Object.fromEntries(value.map);
+
+			res.status(200).send({
+				id: value.id,
+				ip: value.ip,
+				data: plainObject,
+			});
 			return;
 		}
 	}
@@ -15,12 +22,25 @@ nodeRouter.get("/:id", (req, res) => {
 
 nodeRouter.delete("/", (req, res) => {
 	const { nodeId } = req.body;
-	for (const values of storageNodeMap.values()) {
-		if (values.getId() === nodeId) {
-			storageNodeMap.delete(hash(values.getIp()));
-			lookUpArray.splice(lookUpArray.indexOf(hash(values.getIp())), 1);
-			res.status(200).send("Successfully removed");
-			return;
+
+	for (const node of storageNodeMap.values()) {
+		if (node.id === +nodeId) {
+			try {
+				const tempIndex = lookUpArray.indexOf(hash(node.ip));
+				// transfer data to the next node
+				const nextNode = storageNodeMap.get(lookUpArray[tempIndex + 1]);
+				for (const [key, value] of node.map.entries()) {
+					nextNode.add(key, value);
+				}
+				storageNodeMap.delete(lookUpArray[tempIndex]);
+				lookUpArray.splice(tempIndex, 1);
+				res.status(200).send("Successfully removed");
+				return;
+			} catch (e) {
+				console.log(e);
+				res.status(500).send("Internal server error");
+				return;
+			}
 		}
 	}
 	res.status(404).send("Node not found");
